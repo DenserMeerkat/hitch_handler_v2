eaae10fd2be33f7bc378a99848c3719b73735671
DenserMeerkat <95911940+DenserMeerkat@users.noreply.github.com>
2023-11-06 01:06:36 +0530

    Migrated to ZoomDrawer

--- lib/app/views/screens/common/slider_page.dart
+++ lib/app/views/screens/common/slider_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:gap/gap.dart';
 import 'package:hitch_handler_v2/app/views/settings/theme/color_scheme/blend_slider.dart';
 import 'package:hitch_handler_v2/app/views/settings/theme/color_scheme/color_scheme_scroll.dart';
@@ -9,137 +9,117 @@ import 'package:hitch_handler_v2/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 class SliderPage extends StatelessWidget {
-  final Function(bool)? onDrawerToggle;
+  final ZoomDrawerController zoomDrawerController;
   final bool reverse;
   const SliderPage({
     super.key,
-    required GlobalKey<SliderDrawerState> sliderDrawerKey,
-    this.onDrawerToggle,
+    required this.zoomDrawerController,
     this.reverse = false,
-  }) : _sliderDrawerKey = sliderDrawerKey;
-
-  final GlobalKey<SliderDrawerState> _sliderDrawerKey;
+  });
 
   @override
   Widget build(BuildContext context) {
     ThemeProvider themeProvider = context.watch<ThemeProvider>();
     ThemeMode selectedThemeMode = themeProvider.selectedThemeMode;
-    return GestureDetector(
-      onHorizontalDragEnd: (dragDetail) {
-        if (reverse
-            ? dragDetail.velocity.pixelsPerSecond.dx >= 1
-            : dragDetail.velocity.pixelsPerSecond.dx < 1) {
-          _sliderDrawerKey.currentState?.openSlider();
-          onDrawerToggle?.call(true);
-        } else {
-          _sliderDrawerKey.currentState?.closeSlider();
-          onDrawerToggle?.call(false);
-        }
-      },
-      child: Container(
-        alignment: Alignment.topCenter,
-        width: 300.w,
-        color: Theme.of(context).colorScheme.onInverseSurface,
-        child: SafeArea(
-          bottom: false,
-          child: Container(
-            clipBehavior: Clip.hardEdge,
-            width: 280.w,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surface,
-              borderRadius:
-                  const BorderRadius.vertical(top: Radius.circular(8)),
-            ),
-            child: Scaffold(
-              appBar: AppBar(
-                primary: false,
-                automaticallyImplyLeading: false,
-                elevation: 0,
-                title: Text(
-                  "Settings",
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0.8,
+    return Container(
+      alignment: Alignment.topCenter,
+      width: 300.w,
+      color: Theme.of(context).colorScheme.onInverseSurface,
+      child: SafeArea(
+        bottom: false,
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          width: 280.w,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
+          ),
+          child: Scaffold(
+            appBar: AppBar(
+              primary: false,
+              automaticallyImplyLeading: false,
+              elevation: 0,
+              title: Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0.8,
+                ),
+              ),
+              actions: [
+                IconButton(
+                  tooltip: "Close",
+                  onPressed: () {
+                    zoomDrawerController.close!();
+                  },
+                  icon: const Icon(
+                    Icons.close_rounded,
+                    size: 18,
                   ),
                 ),
-                actions: [
-                  IconButton(
-                    tooltip: "Close",
-                    onPressed: () {
-                      _sliderDrawerKey.currentState!.closeSlider();
-                      onDrawerToggle?.call(false);
-                    },
-                    icon: const Icon(
-                      Icons.close_rounded,
-                      size: 18,
+              ],
+              bottom: bottomLine(context),
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: ColorSchemeScroll(
+                      value: context.read<ThemeProvider>(),
+                      size: 40,
+                      padding: 8,
+                    ),
+                  ),
+                  BlendSlider(
+                    value: context.read<ThemeProvider>(),
+                    showBorder: false,
+                    showColor: false,
+                  ),
+                  const Gap(8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: SegmentedButton<ThemeMode>(
+                      style: OutlinedButton.styleFrom(
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        visualDensity:
+                            const VisualDensity(horizontal: -1, vertical: -1),
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        side: BorderSide(
+                          color: Theme.of(context).dividerColor,
+                          width: 0.6,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      showSelectedIcon: false,
+                      segments: const [
+                        ButtonSegment<ThemeMode>(
+                          value: ThemeMode.system,
+                          icon: Icon(Icons.smartphone_outlined, size: 14),
+                          label: Text("System", style: TextStyle(fontSize: 12)),
+                        ),
+                        ButtonSegment<ThemeMode>(
+                          value: ThemeMode.light,
+                          icon: Icon(Icons.light_mode_outlined, size: 14),
+                          label: Text("Light", style: TextStyle(fontSize: 12)),
+                        ),
+                        ButtonSegment<ThemeMode>(
+                          value: ThemeMode.dark,
+                          icon: Icon(Icons.dark_mode_outlined, size: 14),
+                          label: Text("Dark", style: TextStyle(fontSize: 12)),
+                        ),
+                      ],
+                      selected: <ThemeMode>{selectedThemeMode},
+                      onSelectionChanged: (Set<ThemeMode> newSelection) {
+                        themeProvider
+                            .updateSelectedThemeMode(newSelection.first);
+                      },
                     ),
                   ),
                 ],
-                bottom: bottomLine(context),
-              ),
-              body: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12.0),
-                      child: ColorSchemeScroll(
-                        value: context.read<ThemeProvider>(),
-                        size: 40,
-                        padding: 8,
-                      ),
-                    ),
-                    BlendSlider(
-                      value: context.read<ThemeProvider>(),
-                      showBorder: false,
-                      showColor: false,
-                    ),
-                    const Gap(8),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: SegmentedButton<ThemeMode>(
-                        style: OutlinedButton.styleFrom(
-                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          visualDensity:
-                              const VisualDensity(horizontal: -1, vertical: -1),
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          side: BorderSide(
-                            color: Theme.of(context).dividerColor,
-                            width: 0.6,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        showSelectedIcon: false,
-                        segments: const [
-                          ButtonSegment<ThemeMode>(
-                            value: ThemeMode.system,
-                            icon: Icon(Icons.smartphone_outlined, size: 14),
-                            label:
-                                Text("System", style: TextStyle(fontSize: 12)),
-                          ),
-                          ButtonSegment<ThemeMode>(
-                            value: ThemeMode.light,
-                            icon: Icon(Icons.light_mode_outlined, size: 14),
-                            label:
-                                Text("Light", style: TextStyle(fontSize: 12)),
-                          ),
-                          ButtonSegment<ThemeMode>(
-                            value: ThemeMode.dark,
-                            icon: Icon(Icons.dark_mode_outlined, size: 14),
-                            label: Text("Dark", style: TextStyle(fontSize: 12)),
-                          ),
-                        ],
-                        selected: <ThemeMode>{selectedThemeMode},
-                        onSelectionChanged: (Set<ThemeMode> newSelection) {
-                          themeProvider
-                              .updateSelectedThemeMode(newSelection.first);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
               ),
             ),
           ),
--- lib/app/views/screens/home/user_home_page.dart
+++ lib/app/views/screens/home/user_home_page.dart
@@ -6,8 +6,8 @@ import 'package:hitch_handler_v2/app/views/screens/common/slider_page.dart';
 import 'package:hitch_handler_v2/app/views/widgets/header/app_leading_widget.dart';
 import 'package:hitch_handler_v2/app/views/widgets/buttons/icon_button.dart';
 import 'package:hitch_handler_v2/app/views/widgets/misc/overlay_wrapper.dart';
-import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,8 +17,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final GlobalKey<SliderDrawerState> _sliderDrawerKey =
-      GlobalKey<SliderDrawerState>();
+  final ZoomDrawerController zoomDrawerController = ZoomDrawerController();
   int currentPageIndex = 0;
   bool isDrawerOpen = false;
 
@@ -31,10 +30,16 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void onDrawerToggle(bool value) {
-    setState(() {
-      isDrawerOpen = value;
-    });
+  @override
+  void initState() {
+    super.initState();
+    zoomDrawerController.stateNotifier = ValueNotifier(DrawerState.closed);
+  }
+
+  @override
+  void dispose() {
+    zoomDrawerController.stateNotifier!.dispose();
+    super.dispose();
   }
 
   @override
@@ -42,107 +47,93 @@ class _HomePageState extends State<HomePage> {
     return SystemOverlayWrapper(
       child: WillPopScope(
         onWillPop: () async {
-          if (_sliderDrawerKey.currentState!.isDrawerOpen) {
-            _sliderDrawerKey.currentState!.closeSlider();
-            onDrawerToggle(false);
+          if (zoomDrawerController.isOpen!()) {
+            zoomDrawerController.close!();
           } else if (currentPageIndex != 0) {
             onDestinationChange(0);
           }
           return false;
         },
-        child: SliderDrawer(
-          key: _sliderDrawerKey,
-          slideDirection: SlideDirection.RIGHT_TO_LEFT,
-          appBar: Container(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            color: isDrawerOpen
-                ? Theme.of(context).colorScheme.onInverseSurface
-                : Theme.of(context).appBarTheme.backgroundColor,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).appBarTheme.backgroundColor,
-                borderRadius: isDrawerOpen
-                    ? const BorderRadius.vertical(top: Radius.circular(8))
-                    : null,
-              ),
-              child: AppBar(
-                primary: false,
-                scrolledUnderElevation: 0,
-                centerTitle: true,
-                elevation: 0,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
-                ),
-                leading: const AppLeadingWidget(),
-                actions: [
-                  CustomIconButton(
-                    tooltip: "Toggle Sidebar",
-                    icon: Icon(
-                      Symbols.side_navigation_rounded,
-                      size: 20,
-                      color: Theme.of(context)
-                          .colorScheme
-                          .onSurface
-                          .withOpacity(0.8),
-                    ),
-                    onPressed: () {
-                      if (_sliderDrawerKey.currentState!.isDrawerOpen) {
-                        _sliderDrawerKey.currentState!.closeSlider();
-                        onDrawerToggle(false);
-                      } else {
-                        _sliderDrawerKey.currentState!.openSlider();
-                        onDrawerToggle(true);
-                      }
-                    },
-                  )
-                ],
-                title: Text(
-                  viewTitles[currentPageIndex],
-                  style: TextStyle(
-                    fontSize: 20.sp,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0.8,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .onSurface
-                        .withOpacity(0.8),
-                  ),
-                ),
-              ),
-            ),
+        child: ZoomDrawer(
+          controller: zoomDrawerController,
+          shrinkMainScreen: false,
+          angle: 0,
+          menuScreenWidth: 300.w,
+          mainScreenScale: 0,
+          slideWidth: 300.w,
+          isRtl: true,
+          borderRadius: 0,
+          mainScreenTapClose: true,
+          menuScreen: SliderPage(
+            zoomDrawerController: zoomDrawerController,
           ),
-          sliderOpenSize: 300.w,
-          slider: SliderPage(
-            sliderDrawerKey: _sliderDrawerKey,
-            onDrawerToggle: onDrawerToggle,
-          ),
-          animationDuration: 300,
-          child: GestureDetector(
-            behavior: isDrawerOpen
-                ? HitTestBehavior.opaque
-                : HitTestBehavior.translucent,
-            onTap: () {
-              if (_sliderDrawerKey.currentState!.isDrawerOpen) {
-                _sliderDrawerKey.currentState!.closeSlider();
-                onDrawerToggle(false);
-              }
-            },
+          mainScreen: GestureDetector(
             onHorizontalDragEnd: (dragDetail) {
               if (dragDetail.velocity.pixelsPerSecond.dx < 1) {
-                _sliderDrawerKey.currentState?.openSlider();
-                onDrawerToggle(true);
+                zoomDrawerController.open!();
               } else {
-                _sliderDrawerKey.currentState?.closeSlider();
-                onDrawerToggle(false);
+                zoomDrawerController.close!();
               }
             },
-            child: AbsorbPointer(
-              absorbing: isDrawerOpen,
-              child: Scaffold(
-                body: viewList[currentPageIndex],
-                bottomNavigationBar: HomeBottomBar(
-                  currentPageIndex: currentPageIndex,
-                  onDestinationChange: onDestinationChange,
+            child: ValueListenableBuilder(
+              valueListenable: zoomDrawerController.stateNotifier!,
+              builder: (context, value, child) => Container(
+                color: value == DrawerState.open
+                    ? Theme.of(context).colorScheme.onInverseSurface
+                    : Theme.of(context).colorScheme.surface,
+                child: ClipRRect(
+                  borderRadius:
+                      const BorderRadius.vertical(top: Radius.circular(8)),
+                  child: SafeArea(
+                    bottom: false,
+                    child: Scaffold(
+                      backgroundColor: Colors.transparent,
+                      appBar: AppBar(
+                        primary: true,
+                        scrolledUnderElevation: 0,
+                        centerTitle: true,
+                        elevation: 0,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(8)),
+                        ),
+                        leading: const AppLeadingWidget(),
+                        actions: [
+                          CustomIconButton(
+                            tooltip: "Toggle Sidebar",
+                            icon: Icon(
+                              Symbols.side_navigation_rounded,
+                              size: 20,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSurface
+                                  .withOpacity(0.8),
+                            ),
+                            onPressed: () {
+                              zoomDrawerController.toggle!();
+                            },
+                          )
+                        ],
+                        title: Text(
+                          viewTitles[currentPageIndex],
+                          style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.8,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSurface
+                                .withOpacity(0.8),
+                          ),
+                        ),
+                      ),
+                      body: viewList[currentPageIndex],
+                      bottomNavigationBar: HomeBottomBar(
+                        currentPageIndex: currentPageIndex,
+                        onDestinationChange: onDestinationChange,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
